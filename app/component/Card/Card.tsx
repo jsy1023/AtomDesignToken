7c4b0541a07c760585ec88d7d55a7f066a19d3e1
jsy1023 <83209877+jsy1023@users.noreply.github.com>
2024-12-04 10:20:50 +0900

    Revert "채팅 페이지 레이아웃 구현"
    This reverts commit f08dbf0a40bef05dc3e621a00ea2287920c1e54e.

--- app/component/Card/Card.tsx
+++ app/component/Card/Card.tsx
@@ -1,15 +1,5 @@
-const Card = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div className={`bg-fillCard rounded shadow-md ${className}`}>
-      {children}
-    </div>
-  );
+const Card = ({ children }: { children: React.ReactNode }) => {
+  return <div className="">{children}</div>;
 };
 
 export default Card;
