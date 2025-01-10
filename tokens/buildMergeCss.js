bf8e8628a2d628cc94e0a40c9394b06db57e7425
jsy1023 <83209877+jsy1023@users.noreply.github.com>
2025-01-10 11:35:25 +0900

    build 로직 수정

--- tokens/buildMergeCss.js
+++ tokens/buildMergeCss.js
@@ -1,46 +1,79 @@
-import { readFileSync, writeFileSync } from 'fs';
-import path from 'path';
+import StyleDictionary from "style-dictionary";
+import { register } from "@tokens-studio/sd-transforms";
 
-function processCSS() {
-  // CSS 파일들 읽기
-  const globalCSS = readFileSync(path.join(process.cwd(), 'build', 'global.variables.css'), 'utf8')
-    .replace(':root {', '')
-    .replace(/}\s*$/, '');  // 마지막 } 제거
+import { readFileSync, writeFileSync } from "fs";
+
+import path from "path";
+
+register(StyleDictionary);
+
+// 원본 token.json 파일 읽기
+const tokenFile = readFileSync("token.json", "utf8");
+const tokenData = JSON.parse(tokenFile);
+// console.log(tokenData);
+
+// css 파일 빌드 후 통합하여 global.css에 제공하는 함수
+const processCss = async (tokenSetOrder) => {
+  let globalCSS = "";
+
+  let globalItems = readFileSync(
+    path.join(process.cwd(), "build", `global.variables.css`),
+    "utf8"
+  )
+    .replace(":root {", "")
+    .replace(/}\s*$/, ""); // 마지막 } 제거;
+
+  tokenSetOrder.forEach((tokenValue) => {
+    const filePath = path.join(
+      process.cwd(),
+      "build",
+      `${tokenValue}.variables.css`
+    );
+
+    try {
+      let items = readFileSync(filePath, "utf8")
+        .replace(":root {", "")
+        .replace(/}\s*$/, ""); // 마지막 } 제거;
+
+      // 각 토큰별 css를 구조에 맞게 추가
+      globalCSS += `
+      
+      .${tokenValue} {
+        ${items}
+      }
+      `;
+    } catch (error) {
+      console.log(`파일 읽기 실패:${filePath}`, error);
+    }
+  });
+
+  // tailwind 지시어와 통합
+
+  const combinedCSS = `
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
     
-  const darkCSS = readFileSync(path.join(process.cwd(), 'build', 'dark.variables.css'), 'utf8')
-    .replace(':root {', '')
-    .replace(/}\s*$/, '');
-    
-  const digitalFontCSS = readFileSync(path.join(process.cwd(), 'build', 'digitalFont.variables.css'), 'utf8')
-    .replace(':root {', '')
-    .replace(/}\s*$/, '');
+    @layer base{
+        :root{
+          ${globalItems}
+        }
+      }
 
-  // Tailwind 지시어와 함께 CSS 통합
-  const combinedCSS = `@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-@layer base {
-  :root {
     ${globalCSS}
-  }
-}
-
-.theme-dark {
-  ${darkCSS}
-}
-
-.theme-digitalFont {
-  ${digitalFontCSS}
-}`;
+    `;
 
   // 결과를 app/globals.css에 저장
-  writeFileSync(path.join(process.cwd(), 'app', 'globals.css'), combinedCSS);
-}
+  writeFileSync(
+    path.join(process.cwd(), "app", "globals.css"),
+    combinedCSS,
+    "utf8"
+  );
+  console.log("global.css 파일이 성공적으로 생성되었습니다.");
+};
 
 try {
-  processCSS();
-  console.log('CSS 파일이 성공적으로 생성되었습니다.');
+  processCss(tokenData.$metadata.tokenSetOrder);
 } catch (error) {
-  console.error('CSS 처리 중 오류가 발생했습니다:', error);
+  console.error("css파일 통합 처리중 오류가 발생했습니다:", error);
 }
