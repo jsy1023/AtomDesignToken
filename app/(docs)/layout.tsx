176a95214acc8a82f3b5f798268622e13791d076
jsy1023 <83209877+jsy1023@users.noreply.github.com>
2025-01-15 09:24:44 +0900

    Footer 추가

--- app/(docs)/layout.tsx
+++ app/(docs)/layout.tsx
@@ -86,10 +86,21 @@ const DocsLayout = ({ children }: { children: React.ReactNode }) => {
           menu
         </span>
       </button>
-      <div className="w-full bg-fill-wrapper text-text-standard flex justify-center relative overflow-auto">
+      <div className="w-full bg-fill-wrapper text-text-standard flex flex-col items-center relative overflow-auto">
         <div className="max-w-[800px] w-full px-4 py-12 lg:px-0 lg:py-24">
           {children}
         </div>
+        <footer className="w-full py-4 px-8 border-t border-fill-border">
+          Built by{" "}
+          <Link
+            href={"https://www.atomground.com"}
+            target="_blank"
+            className="text-primary"
+          >
+            atomground
+          </Link>
+          . MIT License
+        </footer>
       </div>
     </ThemeWrapper>
   );
