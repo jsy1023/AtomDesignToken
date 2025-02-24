70f815a5a4bcd2b596d17a4966d87f13193e22bf
SY <sy.jeoung1023@gmail.com>
2025-02-24 17:16:30 +0900

    설치하기 가이드 작성

--- app/(docs)/install/page.tsx
+++ app/(docs)/install/page.tsx
@@ -15,14 +15,26 @@ const DocsInstall = () => {
       <h1 className="mb-4 text-h1">설치하기</h1>
       <p className="mb-2">
         &nbsp; npm 명령어를 통해 다운로드 atomsystem을 다운로드 할 수 있습니다.
-        atomsystem은 react, next.js library 입니다. 설치 뒤 다운로드해도 되며
-        npx 명령어를 통해 생성할 수 있습니다.
+      </p>
+      <Card>
+        <CardContent noMargin>
+          <CodeBlock language="npm">{`npm i atomsystem`}</CodeBlock>
+        </CardContent>
+      </Card>
+      <hr className="my-4 border-fill-border" />
+      <p className="mb-2">
+        &nbsp; atomsystem은 react, next.js, tailwinds css를 의존하는 library
+        입니다. <br /> 설치 뒤 다운로드해도 되며 npx 명령어를 통해 라이브러리를
+        생성할 수 있습니다.
       </p>
       <Card>
         <CardContent noMargin>
           <CodeBlock language="npm">{`npx atomsystem-init`}</CodeBlock>
         </CardContent>
       </Card>
+      <p className="my-2">
+        &nbsp; npm init을 통해 token.json 파일과 build 로직을 설치합니다.
+      </p>
       <hr className="my-4 border-fill-border" />
       <p className="mb-2">
         &nbsp; 컴포넌트는 기본적으로 app/components 폴더에 일괄적으로
@@ -34,6 +46,9 @@ const DocsInstall = () => {
           <CodeBlock language="npm">{`npx atomsystem-add`}</CodeBlock>
         </CardContent>
       </Card>
+      <p className="my-2">
+        &nbsp; 각 페이지에서 컴포넌트를 개별적으로 다운로드 받을 수 있습니다.
+      </p>
       <hr className="my-4 border-fill-border" />
       <ul className="list-disc p-8">
         <li>
