a6dbaeae6a4e2d26544afb31379550ad150c00bd
SY <sy.jeoung1023@gmail.com>
2025-07-24 15:25:40 +0900

    todo list 제거

--- app/(docs)/layout.tsx
+++ app/(docs)/layout.tsx
@@ -10,24 +10,6 @@ import { Modal } from "../templates/Modal/Modal";
 const DocsLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      {/**
-       * ✅ 모달 창 컴포넌트 추가 [완료: 2025/06/09]
-       * ✅ 태그 컴포넌트 추가 [완료: 2025/06/10]
-       * ✅ 네비게이션 라벨 타입 추가? = 예외 처리로 하기에는....? [완료: 2025/06/11]
-       * ✅ 페이지네이션 컴포넌트 추가 [완료: 2025/06/12]
-       * ✅ version 0.1.21 배포 [완료: 2025/06/12]
-       * ✅ 콜랩스 컴포넌트 추가 [완료: 2025/06/16]
-       * ✅ TOC 컴포넌트 추가 [완료: 2025/06/18]
-       * ✅ design token의 대한 내용 작성 필요 (theory✅, color✅, spacing✅, typography✅) [완료: 2025/07/21]
-       * ✅ npx download test 및 수정 [완료: 2025/07/24]
-       * contact 페이지 추가
-       * code block 수정
-       * card 컴포넌트 추가
-       * Animations.tsx에 대한 내용 작성 필요
-       * Calendar 라이브러리 구현
-       * chart 라이브러리 구현
-       * table 컴포넌트 (정렬, 그리드, 간격조정 기능 추가)
-       *   */}
       <GlobalNav
         navMenu={[
           { name: "시작하기", label: true },
