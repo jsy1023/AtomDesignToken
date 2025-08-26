28135ff5180abf97535082d7a621ce75672a8616
SY <sy.jeoung1023@gmail.com>
2025-08-26 14:27:31 +0900

    Update CodeBlock.tsx

--- app/templates/CodeBlock/CodeBlock.tsx
+++ app/templates/CodeBlock/CodeBlock.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import hljs from "highlight.js";
 import "highlight.js/styles/atom-one-dark.css"; // 스타일 파일
 
@@ -12,10 +12,16 @@ const CodeBlock = ({
 }) => {
   const codeRef = useRef<HTMLElement | null>(null);
   const [copied, setCopied] = useState(false);
+  const [isHeightCheck, setHeightCheck] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (codeRef.current) {
       hljs.highlightElement(codeRef.current); // 코드 블록 하이라이트
+
+      if (codeRef.current.offsetHeight >= 600) {
+        setHeightCheck(true);
+      }
     }
   }, [children]);
 
@@ -33,7 +39,7 @@ const CodeBlock = ({
 
   return (
     <div className="relative">
-      <pre>
+      <pre className={`${isOpen ? "max-h-fit" : "max-h-60"}  overflow-hidden`}>
         <code ref={codeRef} className={language}>
           {children}
         </code>
@@ -41,10 +47,20 @@ const CodeBlock = ({
 
       <button
         onClick={handleCopy}
-        className="absolute top-2 right-2 bg-white text-black text-xs px-2 py-1 rounded hover:bg-gray-200"
+        className="absolute top-2 right-2 bg-[var(--background-card)] hover:bg-[var(--background-wrapper)] text-[var(--text-standard)] text-xs px-2 py-1 rounded cursor-pointer "
       >
         {copied ? "Copied!" : "Copy"}
       </button>
+
+      {isHeightCheck ? (
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="
+         w-full cursor-pointer hover:bg-[var(--background-wrapper)] text-center py-2 px-8"
+        >
+          {isOpen ? "Close" : "Open"}
+        </button>
+      ) : null}
     </div>
   );
 };
