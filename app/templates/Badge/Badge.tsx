26360c539805f913665657554441280d8fddbaf1
SY <sy.jeoung1023@gmail.com>
2026-01-30 16:15:07 +0900

    badge design token화

--- app/templates/Badge/Badge.tsx
+++ app/templates/Badge/Badge.tsx
@@ -2,18 +2,16 @@ export const Badge = ({
   children,
   color,
   type,
-  className,
 }: {
   children: React.ReactNode;
   color: "primary" | "secondary" | "warning" | "danger";
   type?: "default" | "tint";
-  className?: string;
 }) => {
   switch (type) {
     case "tint":
       return (
         <span
-          className={`text-sm px-1.5 rounded ${color === "primary" ? "bg-primary/20 !text-primary" : color === "secondary" ? "bg-secondary/20 !text-secondary" : color === "warning" ? "bg-warning/20 !text-warning" : color === "danger" ? "bg-danger/20 !text-danger" : null} ${className}`}
+          className={`badge-tint ${color}`}
         >
           {children}
         </span>
@@ -21,7 +19,7 @@ export const Badge = ({
     default:
       return (
         <span
-          className={`text-sm px-1.5 rounded ${color === "primary" ? "bg-primary !text-white" : color === "secondary" ? "bg-secondary !text-white" : color === "warning" ? "bg-warning !text-white" : color === "danger" ? "bg-danger !text-white" : null} ${className}`}
+          className={`badge ${color}`}
         >
           {children}
         </span>
