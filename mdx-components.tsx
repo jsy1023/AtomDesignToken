8a7c07e43c593c371c6b86e39bacc92ec0d981c7
SY <sy.jeoung1023@gmail.com>
2026-03-17 16:48:39 +0900

    하이드레이션 에러 제거

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,13 +3,25 @@ import Image, { ImageProps } from "next/image";
 
 const components = {
   // Allows customizing built-in components, e.g. to add styling.
-  h1: ({ children }) => <h1>{children}</h1>,
-  h2: ({ children }) => <h2>{children}</h2>,
-  h3: ({ children }) => <h3>{children}</h3>,
-  p: ({ children }) => <p>{children}</p>,
-  hr: ({ children }) => <hr className="border-border-standard my-4">{children}</hr>,
-  ul: ({ children }) => <ul className="list-disc pl-6">{children}</ul>,
-  ol: ({ children }) => <ol className="list-decimal pl-6">{children}</ol>,
+  h1: ({ children, ...props }) => <h1 {...props}>{children}</h1>,
+  h2: ({ children, ...props }) => <h2 {...props}>{children}</h2>,
+  h3: ({ children, ...props }) => <h3 {...props}>{children}</h3>,
+  p: ({ children, ...props }) => <p {...props}>{children}</p>,
+  hr: ({ children, ...props }) => (
+    <hr className="border-border-standard my-4" {...props}>
+      {children}
+    </hr>
+  ),
+  ul: ({ children, ...props }) => (
+    <ul className="list-disc pl-6" {...props}>
+      {children}
+    </ul>
+  ),
+  ol: ({ children, ...props }) => (
+    <ol className="list-decimal pl-6" {...props}>
+      {children}
+    </ol>
+  ),
   img: (props) => (
     <Image
       sizes="100vw"
