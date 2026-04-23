65eb4fdee66b7a28217a5126cbbb3a6e9cf3bf59
SY <sy.jeoung1023@gmail.com>
2026-04-23 18:04:46 +0900

    buld 에러 제거

--- app/examples/card/card-demo.tsx
+++ app/examples/card/card-demo.tsx
@@ -8,8 +8,8 @@ export default function CardDemo() {
       <CardContent>카드 콘텐츠: 정보의 메인 내용을 상세하게 배치합니다.</CardContent>
       <CardFooter>
         <div className="flex justify-end gap-2">
-          <Button type="outline" size="sm">취소</Button>
-          <Button type="primary" size="sm">확인</Button>
+          <Button type="outline" size="small">취소</Button>
+          <Button type="primary" size="small">확인</Button>
         </div>
       </CardFooter>
     </Card>
